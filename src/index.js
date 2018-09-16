789fd675443034765557bc4290246f2e50fd092d
provedenceva <anyuta-provedenc@mail.ru>
2018-09-16 14:32:21 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var result = [];
+  var  arr = equation.split('x');
+  var aFromArr = arr[0];
+  var anew = aFromArr.replace(" * ", "");
+  var a = anew.replace(" ", "");
+  var bFromArr = arr[1];
+  var bnew = bFromArr.replace("^2 ", "");
+  var bnewtwo = bnew.replace(" * ", "");
+  var b = bnewtwo.replace(" ", "");
+  var cFromArr = arr[2];
+  var newc = cFromArr.replace(" ", "");
+  var c = newc.replace(" ", "");
+  var D = ((b * b) - (4 * a * c));
+  var kv = Math.round(Math.sqrt(D));
+  var xFirst = (((-1) * b + kv) / (2 * a));
+  var xSecond = (((-1) * b - kv) / (2 * a));
+  result.push(xFirst);
+  result.push(xSecond);
+  result.sort(function(a, b){return a - b});
+
+  return result;// your implementation
 }
